4d31016802798cc5b7814d8980a023324347be14
david <dwting0322@gmail.com>
2022-09-26 23:04:46 -0700

    create image seeder

--- app/models/image.py
+++ app/models/image.py
@@ -7,8 +7,9 @@ class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     url = db.Column(db.String(500), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
     business_id = db.Column(db.Integer, db.ForeignKey('businesses.id'))
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
+    
     
 
     user = db.relationship('User', back_populates='images')
@@ -18,13 +19,13 @@ class Image(db.Model):
     comment = db.relationship('Comment', back_populates='images')
 
 
-    
+
     def to_dict(self):
         return {
             "id": self.id,
             "url": self.url,
             "user_id": self.user_id,
-            "comment_id": self.comment_id,
             "business_id": self.business_id,
+            "comment_id": self.comment_id,
             "user": self.user.to_dict()
         }
