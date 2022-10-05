477b5549c056c723ce1815328c07b066fecf9909
david <dwting0322@gmail.com>
2022-10-05 08:13:08 -0700

    change like color

--- app/models/dislike.py
+++
@@ -1,9 +0,0 @@
-from .db import db
-
-
-dislikes = db.Table(
-    'dislikes',
-    db.Model.metadata,
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-    db.Column('business_id', db.Integer, db.ForeignKey('businesses.id'), primary_key=True)
-)
