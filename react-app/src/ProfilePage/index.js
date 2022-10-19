f4106b6669606523f3f4158e54f7e6c9aac8a4eb
david <dwting0322@gmail.com>
2022-10-18 21:52:58 -0700

    done profile store

---
+++ react-app/src/ProfilePage/index.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { loadUserRequest } from "../store/session";
+loadUserRequest
---
+++ react-app/src/store/profile.js
@@ -0,0 +1,68 @@
+//Types:
+const LOAD_USERPROFILE = "userprofile/LOAD_USERPROFILE";
+const EDIT_USERPROFILE = "userprofile/EDIT_USERPROFILE";
+
+
+
+//Action Creators:
+const loadProfile = (business, profile) => {
+    return {
+      type: LOAD_USERPROFILE,
+      business,
+      profile,
+    };
+  };
+  
+  const editProfile = (userId) => {
+    return {
+      type: EDIT_USERPROFILE,
+      userId,
+    };
+  };
+
+  
+//Thunks:
+export const loadUserProfile = (userId) => async (dispatch) => {
+    const res = await fetch(`/api/profile/${userId}`);
+    if (res.ok) {
+      const profile = await res.json();
+  
+      dispatch(loadProfile(profile.business, profile.profile));
+      return { businesses: profile.business, profile: profile.profile };
+    }
+  };
+  
+  export const editUserProfile = (userId, profile) => async (dispatch) => {
+    const res = await fetch(`/api/profile/edit/${userId}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(profile),
+    });
+    if (res.ok) {
+      const editedUserProfile = await res.json();
+      dispatch(editProfile(editedUserProfile));
+      return editedUserProfile;
+    }
+  };
+  
+  //Initial State:
+  const initialState = {};
+  
+  //Reducer:
+  const userProfileReducer = (state = initialState, action) => {
+    let newState = {};
+    switch (action.type) {
+      case LOAD_USERPROFILE:
+        newState = { ...state };
+        newState.business = action.business;
+        newState.profile = action.profile;
+        return { ...newState };
+      case EDIT_USERPROFILE:
+        newState = action.userId;
+        return { ...newState };
+      default:
+        return state;
+    }
+  };
+  
+  export default userProfileReducer;
